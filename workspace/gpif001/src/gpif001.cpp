582b57e3a28cff2573d6ff1614c5e1dd35817698
fred <fsziegler@gmail.com>
2015-11-30 12:04:29 -0800

    Created base project + versioning

---
+++ workspace/gpif001/src/gpif001.cpp
@@ -0,0 +1,75 @@
+//============================================================================
+// Name        : gpif001.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/*
+gnuplot --jhelp
+aptitude search gnuplot
+sudo apt-get install gnuplotsudo apt-get install
+sudo apt-get install gnuplot
+sudo apt-get install gnuplot
+gnuplot --help
+aptitude search gnuplot
+sudo apt-get install gnuplot
+gnuplot --help
+gnuplot
+gnuplot --help
+gnuplot -h
+gnuplot -V
+gnuplot -persist -e "set title ’Sine curve’; plot sin(x)"
+gnuplot -persist -e "set title `Sine curve`; plot sin(x)"
+gnuplot
+gnuplot -persist -e "set title ’Sine curve’; plot sin(x)"
+gnuplot -persist -e "set title `Sine curve`; plot sin(x)"
+gnuplot -persist -e "plot sin(x)"
+gnuplot -persist -e "set title \"Sine curve\"; plot sin(x)"
+gnuplot -persist -e "set title \"Sine curve\"; plot sin(x)" sin.png
+gnuplot -h
+gnuplot -d
+gnuplot -persist -e "set title \"Sine curve\"; plot sin(x)" > sin.png
+gnuplot -persist -e "set title \"Sine curve\"; plot sin(x)" > sin.plt
+gnuplot sin.dat
+gnuplot
+gnuplot
+gnuplot -persist -e ’plot [-5:5] sinh(x)’
+gnuplot -persist -e "plot [-5:5] sinh(x)"
+gnuplot -persist -e "plot [-5:5] sinh(x)" > sinh.png
+gnuplot -persist -e
+gnuplot
+gnuplot
+gnuplot terminal
+gnuplot -e terminal
+gnuplot
+gnuplot -e set terminal
+gnuplot -e "set terminal"
+gnuplot
+gnuplot
+sudo find / -name gnuplotrc.
+sudo find / -name gnuplotrc
+gnuplot
+history | grep gnuplot
+gnuplot -e "set term png size 600, 400; set output "data1.png"; plot "sin1.dat" with lines"
+gnuplot -e "set term png size 600, 400; set output \"data1.png\"; plot "sin1.dat" with lines"
+gnuplot -e "set term png size 600, 400; set output \"data1.png\"; plot \"sin1.dat\" with lines"
+gnuplot -e "set term png size 600, 400; set output \"gpif.png\"; plot \"sin.dat\" with lines"
+gnuplot -e "set term png size 600, 400; set output "gpif.png"; plot "sin.dat" with lines"
+gnuplot -e "set term png size 600, 400; set output \"gpif.png\"; plot "sin.dat" with lines"
+gnuplot -e "set term png size 600, 400; set output \"data1.png\"; plot \"sin1.dat\" with lines"
+gnuplot
+gnuplot
+gnuplot -e "set term jpg size 600, 400; set output \"gpif000.jpg\"; plot \"sin1.dat\" with lines"
+gnuplot -e "set term jpeg size 600, 400; set output \"gpif000.jpg\"; plot \"sin1.dat\" with lines"
+gnuplot -e "set term jpeg size 600, 400; set output \"gpif000.jpg\"; plot \"sin1.dat\" with lines"
+history | grep gnuplot
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
